d5b02ac4b238f7cbb216349fe7d436b4b96cd8e3
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-22 19:17:23 -0400

    Add Initial Footer To Site

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 //Import of Main Components
 import Header from "./components/Header.js";
 // import Body from "./components/Body.js";
-// import Footer from "./components/Footer.js";
+import Footer from "./components/Footer.js";
 
 const App = () => (
   <div>
     <Header />
-    {/* <Body />
-    <Footer /> */}
+    {/* <Body /> */}
+    <Footer />
   </div>
 );
 
