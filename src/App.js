82668157af5a74d51c73b0da58d5ee5c8044d702
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-22 18:20:54 -0400

    initial commit -react boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//Import of Main Components
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer"
+
+const App = () => {
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+}
+
+export default App;
